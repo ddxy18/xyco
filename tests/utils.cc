ef0c6f4f4056ac3e468e3b0317c27e8bf49383c6
dongxiaoyu <dongxiaoyu18@qq.com>
2021-10-02 21:31:00 +0800

    test: add cases for interface in listener.cc

---
+++ tests/utils.cc
@@ -0,0 +1,67 @@
+#include "utils.h"
+
+#include "utils/result.h"
+
+// co_outer's lifetime is managed by the caller to avoid being destroyed
+// automatically before resumed.
+auto TestRuntimeCtx::co_run(std::function<runtime::Future<void>()> &&co)
+    -> gsl::owner<std::function<runtime::Future<void>()> *> {
+  auto run = []() {
+    auto runtime = runtime::Builder::new_multi_thread()
+                       .worker_threads(1)
+                       .max_blocking_threads(1)
+                       .build()
+                       .unwrap();
+    runtime->run();
+
+    return runtime;
+  };
+
+  static auto runtime = run();
+
+  bool ended = false;
+  auto *co_outer = gsl::owner<std::function<runtime::Future<void>()> *>(
+      new std::function<runtime::Future<void>()>(
+          [&]() -> runtime::Future<void> {
+            co_await co();
+            ended = true;
+          }));
+  runtime->spawn((*co_outer)());
+  while (!ended) {
+  }
+
+  return co_outer;
+}
+
+auto TestRuntimeCtx::co_run_no_wait(std::function<runtime::Future<void>()> &&co)
+    -> TestRuntimeCtxGuard {
+  auto *co_outer = gsl::owner<std::function<runtime::Future<void>()> *>(
+      new std::function<runtime::Future<void>()>(
+          [=]() -> runtime::Future<void> { co_await co(); }));
+
+  return {co_outer, true};
+}
+
+auto TestRuntimeCtx::co_run_without_runtime(
+    std::function<runtime::Future<void>()> &&co) -> TestRuntimeCtxGuard {
+  auto *co_outer = gsl::owner<std::function<runtime::Future<void>()> *>(
+      new std::function<runtime::Future<void>()>(
+          [=]() -> runtime::Future<void> { co_await co(); }));
+
+  return {co_outer, false};
+}
+
+TestRuntimeCtxGuard::TestRuntimeCtxGuard(
+    gsl::owner<std::function<runtime::Future<void>()> *> co_wrapper,
+    bool in_runtime)
+    : co_wrapper_(co_wrapper) {
+  auto rt = in_runtime ? runtime::Builder::new_multi_thread().build().unwrap()
+                       : nullptr;
+  runtime::RuntimeCtx::set_ctx(rt.get());
+  (*co_wrapper_)();
+}
+
+TestRuntimeCtxGuard::~TestRuntimeCtxGuard() {
+  runtime::RuntimeCtx::set_ctx(nullptr);
+  delete co_wrapper_;
+}
---
+++ tests/utils.h
@@ -0,0 +1,45 @@
+#ifndef XYWEBSERVER_TEST_UTILS_H_
+#define XYWEBSERVER_TEST_UTILS_H_
+
+#include <gtest/gtest.h>
+
+#include <functional>
+#include <gsl/pointers>
+
+#include "event/runtime/future.h"
+#include "event/runtime/runtime.h"
+
+#define CO_ASSERT_EQ(val1, val2)               \
+  ({                                           \
+    auto f = [&]() { ASSERT_EQ(val1, val2); }; \
+    f();                                       \
+  })
+
+class Void;
+
+class TestRuntimeCtxGuard {
+ public:
+  TestRuntimeCtxGuard(
+      gsl::owner<std::function<runtime::Future<void>()> *> co_wrapper,
+      bool in_runtime);
+
+  ~TestRuntimeCtxGuard();
+
+ private:
+  gsl::owner<std::function<runtime::Future<void>()> *> co_wrapper_;
+};
+
+class TestRuntimeCtx {
+ public:
+  // run until co finished
+  static auto co_run(std::function<runtime::Future<void>()> &&co)
+      -> gsl::owner<std::function<runtime::Future<void>()> *>;
+
+  static auto co_run_no_wait(std::function<runtime::Future<void>()> &&co)
+      -> TestRuntimeCtxGuard;
+
+  static auto co_run_without_runtime(
+      std::function<runtime::Future<void>()> &&co) -> TestRuntimeCtxGuard;
+};
+
+#endif  // XYWEBSERVER_TEST_UTILS_H_
