122a86af1e611813853c37297e303ee231b2a7f1
dongxiaoyu <dongxiaoyu18@qq.com>
2021-10-03 17:28:00 +0800

    refactor: move Future<void> impl to .cc file

---
+++ src/event/runtime/future.cc
@@ -0,0 +1,61 @@
+#include "future.h"
+
+auto runtime::Future<void>::PromiseType::get_return_object() -> Future<void> {
+  return Future(Handle<promise_type>::from_promise(*this));
+}
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+auto runtime::Future<void>::PromiseType::initial_suspend() noexcept
+    -> InitFuture {
+  return {};
+}
+
+auto runtime::Future<void>::PromiseType::final_suspend() noexcept
+    -> FinalAwaitable {
+  return {waiting_};
+}
+
+auto runtime::Future<void>::PromiseType::unhandled_exception() -> void {}
+
+auto runtime::Future<void>::PromiseType::return_void() -> void {}
+
+runtime::Future<void>::Awaitable::Awaitable(Future<void> &future) noexcept
+    : future_(future) {}
+
+[[nodiscard]] auto runtime::Future<void>::Awaitable::await_ready()
+    const noexcept -> bool {
+  if (future_.self_) {
+    return future_.self_.done();
+  }
+  return false;
+}
+
+auto runtime::Future<void>::Awaitable::await_suspend(
+    Handle<void> waiting_coroutine) noexcept -> bool {
+  future_.waiting_ = waiting_coroutine;
+  // async function's return type
+  if (future_.self_) {
+    future_.self_.promise().waiting_ = waiting_coroutine;
+    return true;
+  }
+  // Future object
+  auto res = future_.poll(waiting_coroutine);
+  return std::holds_alternative<Pending>(res);
+}
+
+auto runtime::Future<void>::Awaitable::await_resume() -> void {}
+
+runtime::Future<void>::Future(Handle<promise_type> self) : self_(self) {}
+
+auto runtime::Future<void>::operator co_await() -> Awaitable {
+  return Awaitable{*this};
+}
+
+[[nodiscard]] auto runtime::Future<void>::poll(Handle<void> self)
+    -> Poll<void> {
+  return Pending();
+}
+
+[[nodiscard]] auto runtime::Future<void>::poll_wrapper() -> bool {
+  return !std::holds_alternative<Pending>(poll(waiting_));
+}
