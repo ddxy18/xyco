84158aee5133c67e12ee0af96f59e014dc4fea83
dongxiaoyu <dongxiaoyu18@qq.com>
2021-10-04 16:25:56 +0800

    feat: support void type Result

--- src/utils/result.h
+++ src/utils/result.h
@@ -1,20 +1,16 @@
 #ifndef XYWEBSERVER_UTILS_RESULT_H_
 #define XYWEBSERVER_UTILS_RESULT_H_
 
-#include <optional>
+#include <variant>
 
 #include "panic.h"
 #include "spdlog/fmt/bundled/format.h"
 #include "utils/logger.h"
 
 template <typename T, typename E>
-concept AllNonVoid = (!std::is_void<T>::value && !std::is_void<E>::value);
+class Result;
 
 class Void {};
-
-template <typename T, typename E>
-requires AllNonVoid<T, E>
-class Result;
 
 template <typename T>
 concept Printable = requires(T val) {
@@ -23,101 +19,312 @@ concept Printable = requires(T val) {
 
 template <typename T, typename E>
 auto Ok(T inner) -> Result<T, E>
-requires AllNonVoid<T, E>;
+requires(!std::is_same_v<T, void>);
 
-template <typename T, typename E>
-auto Ok() -> Result<T, E>
-requires(std::is_same<T, Void>::value&& AllNonVoid<T, E>);
+template <typename E>
+auto Ok() -> Result<void, E>;
 
 template <typename T, typename E>
 auto Err(E inner) -> Result<T, E>
-requires AllNonVoid<T, E>;
+requires(!std::is_same_v<E, void>);
+
+template <typename T>
+auto Err() -> Result<T, void>;
 
 template <typename T, typename E>
-auto Err() -> Result<T, E>
-requires(std::is_same<E, Void>::value&& AllNonVoid<T, E>);
-
-template <typename T, typename E>
-requires AllNonVoid<T, E>
 class Result {
   friend auto Ok<T, E>(T inner) -> Result<T, E>
-  requires AllNonVoid<T, E>;
+  requires(!std::is_same_v<T, void>);
   friend auto Err<T, E>(E inner) -> Result<T, E>
-  requires AllNonVoid<T, E>;
+  requires(!std::is_same_v<E, void>);
 
- public : Result() = default;
+ public:
+  Result() = default;
+
   auto unwrap() -> T requires Printable<E> {
-    if (ok_.has_value()) {
-      return std::move(*ok_);
+    if (inner_.index() == 1) {
+      TRACE("unwrap err:{}\n", std::get<1>(inner_));
+      panic();
     }
-    TRACE("unwrap err:{}\n", *err_);
-    panic();
-    return std::move(*ok_);  // unreachable
+    return std::move(std::get<0>(inner_));
   }
 
   auto unwrap_or(T default_val) -> T {
-    if (ok_.has_value()) {
-      return std::move(*ok_);
+    if (inner_.index() == 0) {
+      return std::get<0>(inner_);
     }
     return default_val;
   }
 
   auto unwrap_err() -> E {
-    if (err_.has_value()) {
-      return std::move(*err_);
+    if (inner_.index() == 0) {
+      panic();
     }
-    panic();
-    return std::move(*err_);  // unreachable
+    return std::get<1>(inner_);
+    // return std::get<1>(inner_);  // unreachable
   }
 
   template <typename MapT>
-  [[nodiscard]] auto map(const std::function<MapT(T)>& f) -> Result<MapT, E>
-  requires(!std::is_void<MapT>::value) {
-    if (ok_.has_value()) {
-      return Ok<MapT, E>(f(ok_.value()));
+  [[nodiscard]] auto map(const std::function<MapT(T)>& f) -> Result<MapT, E> {
+    if constexpr (std::is_same_v<MapT, void>) {
+      if (inner_.index() == 0) {
+        return Ok<MapT, E>();
+      }
+      return Err<MapT, E>(std::get<1>(inner_));
+    } else {
+      if (inner_.index() == 0) {
+        return Ok<MapT, E>(f(std::get<0>(inner_)));
+      }
+      return Err<MapT, E>(std::get<1>(inner_));
     }
-    return Err<MapT, E>(std::move(*err_));
   }
 
   template <typename MapE>
   [[nodiscard]] auto map_err(const std::function<MapE(E)>& f) -> Result<T, MapE>
   requires(!std::is_void<MapE>::value) {
-    if (err_.has_value()) {
-      return Err<T, MapE>(f(err_.value()));
+    if constexpr (std::is_same_v<MapE, void>) {
+      if (!inner_.index() == 1) {
+        return Err<T, MapE>();
+      }
+      return Ok<T, MapE>(std::get<0>(inner_));
+    } else {
+      if (inner_.index() == 1) {
+        return Err<T, MapE>(f(std::get<1>(inner_)));
+      }
+      return Ok<T, MapE>(std::get<0>(inner_));
     }
-    return Ok<T, MapE>(std::move(*ok_));
   }
 
-  auto is_ok() -> bool { return ok_.has_value(); }
+  auto is_ok() -> bool { return inner_.index() == 0; }
 
   auto is_err() -> bool { return !is_ok(); }
 
  private:
-  explicit Result(std::optional<T> ok, std::optional<E> err)
-      : ok_(std::move(ok)), err_(std::move(err)) {}
+  explicit Result(std::variant<T, E, bool>&& inner)
+      : inner_(std::move(inner)) {}
 
-  std::optional<T> ok_;
-  std::optional<E> err_;
+  std::variant<T, E, bool> inner_{std::in_place_index<2>, true};
+};
+
+template <>
+class Result<void, void> {
+  friend auto Ok<void>() -> Result<void, void>;
+  friend auto Err<void>() -> Result<void, void>;
+
+ public:
+  Result() = default;
+
+  auto unwrap() -> void {
+    if (inner_.index() == 1) {
+      TRACE("unwrap err:{void}\n");
+      panic();
+    }
+  }
+
+  auto unwrap_or() -> void {}
+
+  auto unwrap_err() -> void {
+    if (inner_.index() == 0) {
+      panic();
+    }
+  }
+
+  template <typename MapT>
+  [[nodiscard]] auto map(const std::function<MapT(void)>& f)
+      -> Result<MapT, void>
+  requires(!std::is_void<MapT>::value) {
+    if (inner_.index() == 0) {
+      return Ok<MapT, void>(f());
+    }
+    return Err<MapT>();
+  }
+
+  template <typename MapE>
+  [[nodiscard]] auto map_err(const std::function<MapE(void)>& f)
+      -> Result<void, MapE>
+  requires(!std::is_void<MapE>::value) {
+    if (inner_.index() == 1) {
+      return Err<void, MapE>(f());
+    }
+    return Ok<MapE>();
+  }
+
+  auto is_ok() -> bool { return inner_.index() == 0; }
+
+  auto is_err() -> bool { return !is_ok(); }
+
+ private:
+  explicit Result(std::variant<bool, bool>&& inner) : inner_(inner) {}
+
+  std::variant<bool, bool> inner_;
+};
+
+template <typename T>
+requires(!std::is_same_v<T, void>) class Result<T, void> {
+  friend auto Ok<T, void>(T inner) -> Result<T, void>
+  requires(!std::is_same_v<T, void>);
+  friend auto Err<T>() -> Result<T, void>;
+
+ public:
+  Result() = default;
+
+  auto unwrap() -> T {
+    if (inner_.index() == 1) {
+      TRACE("unwrap err:{void}\n");
+      panic();
+    }
+    return std::get<0>(inner_);
+  }
+
+  auto unwrap_or(T default_val) -> T {
+    return inner_.index() == 0 ? std::get<0>(inner_) : default_val;
+  }
+
+  auto unwrap_err() -> void {
+    if (inner_.index() == 0) {
+      panic();
+    }
+  }
+
+  template <typename MapT>
+  [[nodiscard]] auto map(const std::function<MapT(T)>& f)
+      -> Result<MapT, void> {
+    if constexpr (std::is_same_v<MapT, void>) {
+      if (inner_.index() == 0) {
+        return Ok<void>();
+      }
+      return Err<void>();
+    } else {
+      if (inner_.index() == 0) {
+        return Ok<MapT, void>(f(std::get<0>(inner_)));
+      }
+      return Err<MapT>();
+    }
+  }
+
+  template <typename MapE>
+  [[nodiscard]] auto map_err(const std::function<MapE(void)>& f)
+      -> Result<T, MapE>
+  requires(!std::is_void<MapE>::value) {
+    if (inner_.index() == 1) {
+      return Err<T, MapE>(f());
+    }
+    return Ok<T, MapE>(std::get<0>(inner_));
+  }
+
+  auto is_ok() -> bool { return inner_.index() == 0; }
+
+  auto is_err() -> bool { return !is_ok(); }
+
+ private:
+  explicit Result(std::variant<T, bool>&& ok) : inner_(std::move(ok)) {}
+
+  std::variant<T, bool> inner_{std::in_place_index<1>, true};
+};
+
+template <typename E>
+class Result<void, E> {
+  friend auto Ok<E>() -> Result<void, E>;
+  friend auto Err<void, E>(E inner) -> Result<void, E>
+  requires(!std::is_same_v<E, void>);
+
+ public:
+  Result() = default;
+
+  auto unwrap() -> void {
+    if (inner_.index() == 1) {
+      TRACE("unwrap err:{}\n", std::get<1>(inner_));
+      panic();
+    }
+  }
+
+  auto unwrap_or() -> void {}
+
+  auto unwrap_err() -> E {
+    if (inner_.index() == 0) {
+      panic();
+    }
+    return std::get<1>(inner_);
+  }
+
+  template <typename MapT>
+  [[nodiscard]] auto map(const std::function<MapT(void)>& f) -> Result<MapT, E>
+  requires(!std::is_void<MapT>::value) {
+    if (inner_.index() == 1) {
+      return Err<MapT, E>(std::get<1>(inner_));
+    }
+    return Ok<MapT, E>(f());
+  }
+
+  template <typename MapE>
+  [[nodiscard]] auto map_err(const std::function<MapE(E)>& f)
+      -> Result<void, MapE> {
+    if constexpr (std::is_same_v<MapE, void>) {
+      if (inner_.index() == 1) {
+        return Err<void>();
+      }
+      return Ok<void>();
+    } else {
+      if (inner_.index() == 1) {
+        return Err<void, MapE>(f(std::get<1>(inner_)));
+      }
+      return Ok<MapE>();
+    }
+  }
+
+  auto is_ok() -> bool { return inner_.index() == 0; }
+
+  auto is_err() -> bool { return !is_ok(); }
+
+ private:
+  explicit Result(std::variant<bool, E> inner) : inner_(std::move(inner)) {}
+
+  std::variant<bool, E> inner_;
 };
 
 template <typename T, typename E>
 auto Ok(T inner) -> Result<T, E>
-requires AllNonVoid<T, E> { return Result<T, E>({std::move(inner)}, {}); }
+requires(!std::is_same_v<T, void>) {
+  if constexpr (std::is_same_v<E, void>) {
+    return Result<T, E>(
+        std::variant<T, bool>(std::in_place_index<0>, std::move(inner)));
+  } else {
+    return Result<T, E>{
+        std::variant<T, E, bool>(std::in_place_index<0>, std::move(inner))};
+  }
+}
 
-template <typename T, typename E>
-auto Ok() -> Result<T, E>
-requires(std::is_same<T, Void>::value&& AllNonVoid<T, E>) {
-  return Ok<T, E>(Void{});
+template <typename E>
+auto Ok() -> Result<void, E> {
+  if constexpr (std::is_same_v<E, void>) {
+    return Result<void, E>(
+        std::variant<bool, bool>{std::in_place_index<0>, true});
+  } else {
+    return Result<void, E>(std::variant<bool, E>{std::in_place_index<0>, true});
+  }
 }
 
 template <typename T, typename E>
 auto Err(E inner) -> Result<T, E>
-requires AllNonVoid<T, E> { return Result<T, E>({}, {inner}); }
+requires(!std::is_same_v<E, void>) {
+  if constexpr (std::is_same_v<T, void>) {
+    return Result<T, E>(
+        std::variant<bool, E>{std::in_place_index<1>, std::move(inner)});
 
-template <typename T, typename E>
-auto Err() -> Result<T, E>
-requires(std::is_same<E, Void>::value&& AllNonVoid<T, E>) {
-  return Err<T, E>(Void{});
+  } else {
+    return Result<T, E>{std::move(
+        std::variant<T, E, bool>{std::in_place_index<1>, std::move(inner)})};
+  }
+}
+
+template <typename T>
+auto Err() -> Result<T, void> {
+  if constexpr (std::is_same_v<T, void>) {
+    return Result<T, void>(
+        std::variant<bool, bool>{std::in_place_index<1>, true});
+  } else {
+    return Result<T, void>(std::variant<T, bool>{std::in_place_index<1>, true});
+  }
 }
 
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
--- tests/utils/result_test.cc
+++ tests/utils/result_test.cc
@@ -2,58 +2,93 @@
 
 #include <gtest/gtest.h>
 
-TEST(ResultTest, IsOk) {
-  auto result = Ok<int, int>(1);
-  ASSERT_TRUE(result.is_ok());
-}
-
-TEST(ResultTest, Unwrap) {
-  auto result = Ok<int, int>(1);
-  ASSERT_EQ(result.unwrap(), 1);
-}
-
-TEST(ResultTest, UnwrapErr) {
-  try {
-    Err<int, int>(1).unwrap();
-  } catch (std::runtime_error e) {
-    ASSERT_EQ(std::string(e.what()), "panic");
+class ResultTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    no_void_ok_ = Ok<int, int>(1);
+    no_void_err_ = Err<int, int>(1);
+    void_t_ok_ = Ok<int>();
+    void_t_err_ = Err<void, int>(1);
+    void_e_ok_ = Ok<int, void>(1);
+    void_e_err_ = Err<int>();
+    void_t_void_e_ok_ = Ok<void>();
+    void_t_void_e_err_ = Err<void>();
   }
+
+ public:
+  Result<int, int> no_void_ok_;
+  Result<int, int> no_void_err_;
+  Result<void, int> void_t_ok_;
+  Result<void, int> void_t_err_;
+  Result<int, void> void_e_ok_;
+  Result<int, void> void_e_err_;
+  Result<void, void> void_t_void_e_ok_;
+  Result<void, void> void_t_void_e_err_;
+};
+
+TEST_F(ResultTest, Isok) {
+  ASSERT_TRUE(no_void_ok_.is_ok());
+  ASSERT_TRUE(no_void_err_.is_err());
+  ASSERT_TRUE(void_t_ok_.is_ok());
+  ASSERT_TRUE(void_t_err_.is_err());
+  ASSERT_TRUE(void_e_ok_.is_ok());
+  ASSERT_TRUE(void_e_err_.is_err());
+  ASSERT_TRUE(void_t_void_e_ok_.is_ok());
+  ASSERT_TRUE(void_t_void_e_err_.is_err());
 }
 
-TEST(ResultTest, UnwrapOr) {
-  auto result = Err<std::string, int>(0).unwrap_or(std::string("ab"));
-  ASSERT_EQ(result.length(), 2);
+TEST_F(ResultTest, Unwrap) {
+  ASSERT_EQ(no_void_ok_.unwrap(), 1);
+  ASSERT_EQ(no_void_err_.unwrap_err(), 1);
+  ASSERT_NO_THROW(void_t_ok_.unwrap());
+  ASSERT_EQ(void_t_err_.unwrap_err(), 1);
+  ASSERT_EQ(void_e_ok_.unwrap(), 1);
+  ASSERT_NO_THROW(void_e_err_.unwrap_err());
+  ASSERT_NO_THROW(void_t_void_e_ok_.unwrap());
+  ASSERT_NO_THROW(void_t_void_e_err_.unwrap_err());
 }
 
-TEST(ResultTest, Map) {
-  auto result = Ok<int, int>(1);
-  ASSERT_EQ(
-      result.map(std::function<const char *(int)>([](auto n) { return "a"; }))
-          .unwrap(),
-      "a");
+TEST_F(ResultTest, UnwrapOr) {
+  ASSERT_EQ(no_void_ok_.unwrap_or(-1), 1);
+  ASSERT_EQ(no_void_err_.unwrap_or(-1), -1);
+  void_t_ok_.unwrap_or();
+  void_t_err_.unwrap_or();
+  ASSERT_EQ(void_e_ok_.unwrap_or(-1), 1);
+  ASSERT_EQ(void_e_err_.unwrap_or(-1), -1);
+  void_t_void_e_ok_.unwrap_or();
+  void_t_void_e_err_.unwrap_or();
 }
 
-TEST(ResultTest, MapErr) {
-  auto result = Err<int, int>(1);
-  ASSERT_EQ(
-      result
-          .map_err(std::function<const char *(int)>([](auto n) { return "a"; }))
-          .unwrap_err(),
-      "a");
+TEST_F(ResultTest, Map) {
+  auto map_int_f = std::function<const char *(int)>([](auto n) { return "a"; });
+  auto map_void_f = std::function<const char *(void)>([]() { return "a"; });
+
+  ASSERT_EQ(no_void_ok_.map(map_int_f).unwrap(), "a");
+  ASSERT_EQ(no_void_err_.map(map_int_f).unwrap_err(), 1);
+  ASSERT_EQ(void_t_ok_.map(map_void_f).unwrap(), "a");
+  ASSERT_EQ(void_t_err_.map(map_void_f).unwrap_err(), 1);
+  ASSERT_EQ(void_e_ok_.map(map_int_f).unwrap(), "a");
+  ASSERT_NO_THROW(void_e_err_.map(map_int_f).unwrap_err());
+  ASSERT_EQ(void_t_void_e_ok_.map(map_void_f).unwrap(), "a");
+  ASSERT_NO_THROW(void_t_void_e_err_.map(map_void_f).unwrap_err());
 }
 
-TEST(ResultTest, Pointer) {
-  auto ok = std::make_unique<int>(1);
-  auto result = Ok<int *, int>(ok.get());
+TEST_F(ResultTest, MapErr) {
+  auto map_int_f = std::function<const char *(int)>([](auto n) { return "a"; });
+  auto map_void_f = std::function<const char *(void)>([]() { return "a"; });
+
+  ASSERT_EQ(no_void_ok_.map_err(map_int_f).unwrap(), 1);
+  ASSERT_EQ(no_void_err_.map_err(map_int_f).unwrap_err(), "a");
+  ASSERT_NO_THROW(void_t_ok_.map_err(map_int_f).unwrap());
+  ASSERT_EQ(void_t_err_.map_err(map_int_f).unwrap_err(), "a");
+  ASSERT_EQ(void_e_ok_.map_err(map_void_f).unwrap(), 1);
+  ASSERT_EQ(void_e_err_.map_err(map_void_f).unwrap_err(), "a");
+  ASSERT_NO_THROW(void_t_void_e_ok_.map_err(map_void_f).unwrap());
+  ASSERT_EQ(void_t_void_e_err_.map_err(map_void_f).unwrap_err(), "a");
+}
+
+TEST_F(ResultTest, Pointer) {
+  auto value = std::make_unique<int>(1);
+  auto result = Ok<int *, int>(value.get());
   ASSERT_EQ(*result.unwrap(), 1);
-}
-
-TEST(ResultTest, VoidOk) {
-  auto result = Ok<Void, int>(Void());
-  ASSERT_TRUE(result.is_ok());
-}
-
-TEST(ResultTest, VoidErr) {
-  auto result = Err<int, Void>(Void());
-  ASSERT_TRUE(result.is_err());
 }
