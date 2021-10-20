bb2cdac521e2e01590e7d94e2a547dd2270d2828
dongxiaoyu <dongxiaoyu18@qq.com>
2021-10-20 11:04:08 +0800

    feat: impl timeout

---
+++ tests/time/timeout.cc
@@ -0,0 +1,34 @@
+#include "time/timeout.h"
+
+#include <gtest/gtest.h>
+
+#include "utils.h"
+
+TEST(TimeoutTest, no_timeout) {
+  TestRuntimeCtx::co_run({[]() -> xyco::runtime::Future<void> {
+    constexpr std::chrono::milliseconds timeout_ms =
+        std::chrono::milliseconds(10);
+
+    auto co_inner = []() -> xyco::runtime::Future<int> { co_return 1; };
+
+    auto result = co_await xyco::time::timeout(timeout_ms, co_inner());
+
+    CO_ASSERT_EQ(result.unwrap(), 1);
+  }});
+}
+
+TEST(TimeoutTest, timeout) {
+  TestRuntimeCtx::co_run({[]() -> xyco::runtime::Future<void> {
+    constexpr std::chrono::milliseconds timeout_ms =
+        std::chrono::milliseconds(10);
+
+    auto co_inner = [&]() -> xyco::runtime::Future<int> {
+      co_await xyco::time::sleep(timeout_ms + std::chrono::milliseconds(1));
+      co_return 1;
+    };
+
+    auto result = co_await xyco::time::timeout(timeout_ms, co_inner());
+
+    CO_ASSERT_EQ(result.is_err(), true);
+  }});
+}
