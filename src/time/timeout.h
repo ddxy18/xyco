ae3793344d5ce667dd67eaf9557859535f9f11b4
dongxiaoyu <dongxiaoyu18@qq.com>
2021-10-20 20:07:59 +0800

    refactor: impl time::timeout by runtime::select

--- src/time/timeout.h
+++ src/time/timeout.h
@@ -1,34 +1,20 @@
 #ifndef XYCO_TIME_TIMEOUT_H
 #define XYCO_TIME_TIMEOUT_H
 
+#include "runtime/utils/select.h"
 #include "sleep.h"
 
 namespace xyco::time {
-// FIXME(dongxiaoyu): Cancel the future if timeout. Return immediately when
-// future completes.
+// FIXME(dongxiaoyu): Cancel the future if timeout.
 template <typename T, typename Rep, typename Ratio>
 auto timeout(std::chrono::duration<Rep, Ratio> duration,
              runtime::Future<T> future) -> runtime::Future<Result<T, void>> {
   using CoOutput = Result<T, void>;
 
-  std::optional<T> return_value;
-  auto future_wrapper =
-      [&](runtime::Future<T> future) -> runtime::Future<void> {
-    return_value = co_await future;
-  };
+  auto result = co_await runtime::select(future, sleep(duration));
 
-  auto new_future = future_wrapper(future);
-
-  if (return_value.has_value()) {
-    co_return CoOutput::ok(return_value.value());
-  }
-
-  co_await sleep(duration);
-  if (return_value.has_value()) {
-    co_return CoOutput::ok(return_value.value());
-  }
-
-  co_return CoOutput::err();
+  co_return result.index() == 0 ? CoOutput::ok(std::get<0>(result).inner_)
+                                : CoOutput::err();
 }
 }  // namespace xyco::time
 
