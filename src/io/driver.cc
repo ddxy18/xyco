28d53442a570c5ce568e1ea1b1c77c47f6e353af
dongxiaoyu <dongxiaoyu18@qq.com>
2021-10-22 20:29:45 +0800

    feat: remove nodiscard in .cc

--- src/io/driver.cc
+++ src/io/driver.cc
@@ -3,46 +3,49 @@
 #include "runtime/future.h"
 #include "runtime/runtime.h"
 
-[[nodiscard]] auto xyco::io::IoRegistry::Register(runtime::Event& ev,
-                                                  runtime::Interest interest)
+auto xyco::io::IoRegistry::Register(runtime::Event& ev,
+                                    runtime::Interest interest)
     -> IoResult<void> {
   return register_local(ev, interest);
 }
 
-[[nodiscard]] auto xyco::io::IoRegistry::reregister(runtime::Event& ev,
-                                                    runtime::Interest interest)
+auto xyco::io::IoRegistry::reregister(runtime::Event& ev,
+                                      runtime::Interest interest)
     -> IoResult<void> {
   return reregister_local(ev, interest);
 }
 
-[[nodiscard]] auto xyco::io::IoRegistry::deregister(runtime::Event& ev,
-                                                    runtime::Interest interest)
+auto xyco::io::IoRegistry::deregister(runtime::Event& ev,
+                                      runtime::Interest interest)
     -> IoResult<void> {
   return deregister_local(ev, interest);
 }
 
-[[nodiscard]] auto xyco::io::IoRegistry::register_local(
-    runtime::Event& ev, runtime::Interest interest) -> IoResult<void> {
+auto xyco::io::IoRegistry::register_local(runtime::Event& ev,
+                                          runtime::Interest interest)
+    -> IoResult<void> {
   auto current_thread =
       runtime::RuntimeCtx::get_ctx()->workers_.find(std::this_thread::get_id());
   return current_thread->second->get_epoll_registry().Register(ev, interest);
 }
 
-[[nodiscard]] auto xyco::io::IoRegistry::reregister_local(
-    runtime::Event& ev, runtime::Interest interest) -> IoResult<void> {
+auto xyco::io::IoRegistry::reregister_local(runtime::Event& ev,
+                                            runtime::Interest interest)
+    -> IoResult<void> {
   auto current_thread =
       runtime::RuntimeCtx::get_ctx()->workers_.find(std::this_thread::get_id());
   return current_thread->second->get_epoll_registry().reregister(ev, interest);
 }
 
-[[nodiscard]] auto xyco::io::IoRegistry::deregister_local(
-    runtime::Event& ev, runtime::Interest interest) -> IoResult<void> {
+auto xyco::io::IoRegistry::deregister_local(runtime::Event& ev,
+                                            runtime::Interest interest)
+    -> IoResult<void> {
   auto current_thread =
       runtime::RuntimeCtx::get_ctx()->workers_.find(std::this_thread::get_id());
   return current_thread->second->get_epoll_registry().deregister(ev, interest);
 }
 
-[[nodiscard]] auto xyco::io::IoRegistry::select(runtime::Events& events,
-                                                int timeout) -> IoResult<void> {
+auto xyco::io::IoRegistry::select(runtime::Events& events, int timeout)
+    -> IoResult<void> {
   return IoResult<void>::ok();
 }
