7da8c2b8f712c9a5f57b2de8166552d490726b49
ddxy18 <dongxiaoyu18@qq.com>
2022-10-07 16:15:04 +0000

    [Chore] Drop commit message check (#57)
    # Feature
    - Switch PR merge strategy to squash merge.
    - Drop commit message format check.

--- tools/git-hooks/src/main.rs
+++ tools/git-hooks/src/main.rs
@@ -1,23 +1,19 @@
 #![feature(assert_matches)]
 
-mod commit_msg;
 mod pre_commit;
 
-use self::commit_msg::CommitMessage;
 use self::pre_commit::SubCheck;
 
 use anyhow::Result;
 
 #[derive(structopt::StructOpt)]
 enum Command {
-    CommitMessage(CommitMessage),
     Check(SubCheck),
 }
 
 fn main() -> Result<()> {
     let cmd: Command = <Command as structopt::StructOpt>::from_args();
     match cmd {
-        Command::CommitMessage(msg) => msg.check(),
         Command::Check(check_args) => check_args.check(),
     }
 }
