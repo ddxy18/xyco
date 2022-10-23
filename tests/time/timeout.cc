4abc3f33de698562293d92a5e00def43c566ce82
ddxy18 <dongxy.18@gmail.com>
2022-10-23 23:14:45 +0800

    [Refactor] Time wheel (#65)
    # Feature
    - Replace mocking step by step walking with jumping to current slots in
    every poll.
    - Rearrange next slot's events in every poll to distinguish executing
    sequence of events in higher levels.

--- tests/time/timeout.cc
+++ tests/time/timeout.cc
@@ -33,6 +33,22 @@ TEST(TimeoutTest, timeout) {
   });
 }
 
+TEST(TimeoutTest, longer_timeout) {
+  TestRuntimeCtx::co_run([]() -> xyco::runtime::Future<void> {
+    constexpr std::chrono::milliseconds timeout_ms =
+        std::chrono::milliseconds(6);
+
+    auto co_inner = [&]() -> xyco::runtime::Future<int> {
+      co_await xyco::time::sleep(10 * timeout_ms);
+      co_return 1;
+    };
+
+    auto result = co_await xyco::time::timeout(11 * timeout_ms, co_inner());
+
+    CO_ASSERT_EQ(result.is_ok(), true);
+  });
+}
+
 TEST(TimeoutTest, void_future) {
   TestRuntimeCtx::co_run([]() -> xyco::runtime::Future<void> {
     constexpr std::chrono::milliseconds timeout_ms =
