2a4651e6055945f13b1ea2df9f0d0ee92708454b
Xiaoyu Dong <dongxiaoyu18@qq.com>
2023-04-01 15:12:09 +0000

    fix llvm toolchain installation failure

--- scripts/install_llvm.sh
+++ scripts/install_llvm.sh
@@ -2,11 +2,11 @@
 
 LLVM_VERSION=$1
 
-if [[ -z "`apt-key list | grep -i llvm`" ]]; then
+if [[ ! -f /etc/apt/trusted.gpg.d/apt.llvm.org.asc ]]; then
     # download GPG key once
-    wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+    wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
 fi
 
-add-apt-repository 'deb http://apt.llvm.org/jammy/  llvm-toolchain-jammy main'
+add-apt-repository "deb http://apt.llvm.org/jammy/  llvm-toolchain-jammy-$LLVM_VERSION main"
 apt-get update
 apt-get install -y clang-$LLVM_VERSION lld-$LLVM_VERSION clang-tidy-$LLVM_VERSION clang-format-$LLVM_VERSION llvm-$LLVM_VERSION-dev lld-$LLVM_VERSION libc++-$LLVM_VERSION-dev libc++abi-$LLVM_VERSION-dev libunwind-$LLVM_VERSION-dev
