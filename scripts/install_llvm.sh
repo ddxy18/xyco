be9c697ab7ef2f6224f9bb4de535238ba1a4e189
Xiaoyu Dong <dongxiaoyu18@qq.com>
2023-05-01 06:57:41 +0000

    use main apt source

--- scripts/install_llvm.sh
+++ scripts/install_llvm.sh
@@ -7,6 +7,6 @@ if [[ ! -f /etc/apt/trusted.gpg.d/apt.llvm.org.asc ]]; then
     wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
 fi
 
-add-apt-repository "deb http://apt.llvm.org/jammy/  llvm-toolchain-jammy-$LLVM_VERSION main"
+add-apt-repository "deb http://apt.llvm.org/jammy/  llvm-toolchain-jammy main"
 apt-get update
 apt-get install -y clang-$LLVM_VERSION lld-$LLVM_VERSION clang-tidy-$LLVM_VERSION clang-format-$LLVM_VERSION llvm-$LLVM_VERSION-dev lld-$LLVM_VERSION libc++-$LLVM_VERSION-dev libc++abi-$LLVM_VERSION-dev libunwind-$LLVM_VERSION-dev
