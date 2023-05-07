8af3e64dd52d3c5bf3560fba56599600d309c58c
Xiaoyu Dong <dongxiaoyu18@qq.com>
2023-05-07 09:37:19 +0000

    fix runtime level global registry data race

--- src/runtime/global_registry.h
+++ src/runtime/global_registry.h
@@ -2,6 +2,7 @@
 #define XYCO_RUNTIME_GLOBAL_REGISTRY_H_
 
 #include <gsl/pointers>
+#include <shared_mutex>
 
 #include "runtime/runtime.h"
 
@@ -13,23 +14,36 @@ class GlobalRegistry {
   template <typename... Args>
   static auto get_instance(Args... args) -> std::shared_ptr<R> {
     auto current_runtime = gsl::make_not_null(runtime::RuntimeCtx::get_ctx());
-    if (per_runtime_registries_.contains(current_runtime)) {
-      return per_runtime_registries_[current_runtime];
+    {
+      std::shared_lock<std::shared_mutex> lock_guard(runtime_mutex_);
+      if (per_runtime_registry_.contains(current_runtime)) {
+        return per_runtime_registry_[current_runtime];
+      }
     }
+
     auto registry = std::make_shared<R>(args...);
-    per_runtime_registries_[current_runtime] = registry;
+    {
+      std::scoped_lock<std::shared_mutex> lock_guard(runtime_mutex_);
+      per_runtime_registry_[current_runtime] = registry;
+    }
     return registry;
   }
 
  private:
   static std::unordered_map<runtime::Runtime *, std::shared_ptr<R>>
-      per_runtime_registries_;
+      per_runtime_registry_;
+  // Prevents runtime level data race, worker level multithread is serialized in
+  // the runtime implementation.
+  static std::shared_mutex runtime_mutex_;
 };
 
 template <typename R>
   requires(std::derived_from<R, Registry>)
 std::unordered_map<runtime::Runtime *, std::shared_ptr<R>>
-    GlobalRegistry<R>::per_runtime_registries_;
+    GlobalRegistry<R>::per_runtime_registry_;
+template <typename R>
+  requires(std::derived_from<R, Registry>)
+std::shared_mutex GlobalRegistry<R>::runtime_mutex_;
 }  // namespace xyco::runtime
 
 #endif  // XYCO_RUNTIME_GLOBAL_REGISTRY_H_
